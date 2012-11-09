e39892c525ac638481489469a2d8ce24c1fdee08
Patrick Baselier <patrick@kabisa.nl>
2012-11-09 16:22:31 +0100

    Create clean cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LauyonCms::Application.config.secret_token = '232938c51b39ffd280004a84433eae44ff63446291bde45c7c26b75d0918455c032084bcc5aa46be1e2c327de6ea598e8aab954decbd8fb0ca9a2dbcd7b482eb'
