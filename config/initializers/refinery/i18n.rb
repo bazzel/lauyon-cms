0eae2a8776307403566d449f120347623379ce42
Patrick Baselier <patrick@kabisa.nl>
2012-11-24 09:31:11 +0100

    Use nl as default backend language

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -5,7 +5,7 @@ Refinery::I18n.configure do |config|
 
   # config.default_locale = :en
 
-  # config.current_locale = :en
+   config.current_locale = :nl
 
    config.default_frontend_locale = :nl
 
